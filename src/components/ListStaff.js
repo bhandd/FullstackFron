2d5a5a3d84a7397785ffbf61c0015ccc11c6cfce
Simon Springer <simon.springer1001@gmail.com>
2024-11-17 01:01:00 +0100

    Can now create messages

--- src/components/ListStaff.js
+++ src/components/ListStaff.js
@@ -55,8 +55,8 @@ export default function ListStaffApp(){
                                         <td>{s.name}</td>
                                         <td>{s.role}</td>
                                         <td>{s.social_number}</td>
-                                        <td><Link to="/">
-                                            <button className="update-btn">Update Staff</button>
+                                        <td><Link to={`/createMessage/${s.id}`}>
+                                            <button className="blue-button">Message</button>
                                         </Link></td>
                                         <td><Link to="/">
                                             <button className="delete-btn">Delete Staff</button>
